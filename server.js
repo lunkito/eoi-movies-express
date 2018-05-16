7ce1b95cee9054a04ffea11cfa330f6084453e2a
Pablo <pablo.martingu@gmail.com>
2018-05-16 20:31:10 +0100

    Promesas

--- server.js
+++ server.js
@@ -1,115 +1,19 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
+const express = require('express')
+const app = express()
+const moviesRouter = require('./src/api/movies')
 
-const movies = [
-  {id: 1, name: "Peli 1"},
-  {id: 2, name: "Peli 2"}
-];
+app.use(express.json())
+app.use('/movies', moviesRouter.router)
 
-// GET ---------------------------------
-app.get('/movies', (req, res) => {
-  res.json(movies);
-});
+function loadData() {
+  return Promise.all([
+    moviesRouter.load()
+  ])
+}
 
-app.get('/movie/:id', (req, res) => {
-  const movieId = req.params.id
-  const movie = movies.find(movie => movie.id == movieId);
-
-  if (movie != undefined) {
-    res.json(movie)
-  } else {
-    res.send(404)
-  }
-});
-
-// POST ------------------------------
-app.post('/movies', (req, res) => {
-  const newMovie = req.body
-  const lastMovieId = movies[movies.length - 1].id
-  newMovie.id = lastMovieId + 1
-  movies.push(newMovie)
-  res.json(newMovie)
-});
-
-// PUT --------------------------------
-app.put('/movies', (req, res) => {
-  const movieId = req.body.id
-  const putMovieIndex = movies.findIndex(movie => movie.id == movieId);
-
-  if (putMovieIndex >= 0) {
-    movies[putMovieIndex] = {
-      ...movies[putMovieIndex],
-      ...req.body
-    }
-    res.json(movies[putMovieIndex])
-  } else {
-    res.send(404)
-  }
-});
-
-app.put('/movie/like/:id', (req, res) => {
-  const movieId = req.params.id
-  const putMovieIndex = movies.findIndex(movie => movie.id == movieId);
-
-  if (putMovieIndex >= 0) {
-    // undefined + 1 = undefined 2maneras de arreglarlo
-    movies[putMovieIndex].likes = (movies[putMovieIndex].likes || 0) + 1 // Si movies[] es undefined, sale 0
-    // Lo mismo con if comprimido
-    movies[putMovieIndex].likes = movies[putMovieIndex].likes ? movies[putMovieIndex].likes++ : 1;
-  } else {
-    res.send(404)
-  }
-});
-
-// DELETE ----------------------------
-app.delete('/movie/:id', (req, res) => {
-  const movieId = req.params.id
-  const deleteMovieIndex = movies.findIndex(movie => movie.id == movieId);
-
-  if (deleteMovieIndex >= 0) {
-    const removed = movies.splice(deleteMovieIndex, 1)
-    res.json(removed)
-  } else {
-    res.send(404)
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () => console.log('Ready on port 3000'))
+loadData()
+  .then(() => {
+    app.listen(3000, () => console.log('Ready on port 3000'))    
+  })
+  .catch(() => console.log('No se pudo lanzar el servidor'))
+// app.listen(3000, () => console.log('Ready on port 3000'))
